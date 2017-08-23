98930f70a3f3adbd7e30b8318fa577353baf3f73
Seokseong Jeon <songsong@monet.postech.ac.kr>
2017-08-23 07:16:05 +0000

    Compile success (test, refactor needed)

--- tagRPi/i2cController.cpp
+++ tagRPi/i2cController.cpp
@@ -2,7 +2,12 @@
 #include <cerrno>
 #include <cstdint>
 #include <cstring>
+#include <fstream>
 #include <iostream>
+
+#include <eigen3/Eigen/Dense>
+
+#include <mlat.h>
 
 // I2C
 #include <linux/i2c-dev.h> // I2C_SLAVE
@@ -18,6 +23,8 @@
 #define NUM_ANCHORS 5
 
 using namespace std;
+using namespace Eigen;
+using namespace mlat;
 
 uint8_t cmd_scan = CMD_SCAN;
 uint8_t cmd_data_ready = CMD_DATA_READY;
@@ -54,7 +61,7 @@ int isReady(int i2cFd) {
   }
 }
 
-int getAnchorIds(int i2cFd, uint16_t* anchorId, int num_anchors) {
+int getAnchorIds(int i2cFd, uint16_t* anchorId) {
   uint8_t data[32];
 
   cout << "Getting Anchors' IDs..." << endl;
@@ -62,18 +69,18 @@ int getAnchorIds(int i2cFd, uint16_t* anchorId, int num_anchors) {
     cout << "Writing CMD_TYPE_ID failed" << endl;
     return -1;
   }
-  if (read(i2cFd, data, 2 * num_anchors) != 2 * num_anchors) {
+  if (read(i2cFd, data, 2 * NUM_ANCHORS) != 2 * NUM_ANCHORS) {
     cout << "Reading Anchors' IDs failed" << endl;
     return -1;
   }
-  for (int i = 0; i < num_anchors; i++) {
+  for (int i = 0; i < NUM_ANCHORS; i++) {
     anchorId[i] = ID_NONE;
     /*& Arduino uses little endian */
     anchorId[i] = (data[2 * i + 1] << 8) | data[2 * i + 0];
   }
 }
 
-int getDists(int i2cFd, float* distance, int num_anchors) {
+int getDists(int i2cFd, float* distance) {
   uint8_t data[32];
 
   cout << "Getting distance measurements..." << endl;
@@ -81,11 +88,11 @@ int getDists(int i2cFd, float* distance, int num_anchors) {
     cout << "Writing CMD_TYPE_DIST failed" << endl;
     return -1;
   }
-  if (read(i2cFd, data, 4 * num_anchors) != 4 * num_anchors) {
+  if (read(i2cFd, data, 4 * NUM_ANCHORS) != 4 * NUM_ANCHORS) {
     cout << "Reading distance measurements failed" << endl;
     return -1;
   }
-  for (int i = 0; i < num_anchors; i++) {
+  for (int i = 0; i < NUM_ANCHORS; i++) {
     /* Arduino uses little endian */
     uint32_t float_binary = (data[4 * i + 3] << 24) | (data[4 * i + 2] << 16)
                           | (data[4 * i + 1] <<  8) | (data[4 * i + 0]      );
@@ -94,7 +101,7 @@ int getDists(int i2cFd, float* distance, int num_anchors) {
   }
 }
 
-int readMeasurement(int i2cFd, uint16_t* anchorId, float* distance, int num_anchors) {
+int readMeasurement(int i2cFd, uint16_t* anchorId, float* distance) {
   int ret;
   uint8_t data[32];
 
@@ -103,12 +110,12 @@ int readMeasurement(int i2cFd, uint16_t* anchorId, float* distance, int num_anch
     return ret;
   }
 
-  ret = getAnchorIds(i2cFd, anchorId, num_anchors);
+  ret = getAnchorIds(i2cFd, anchorId);
   if (ret < 0) {
     return ret;
   }
 
-  ret = getDists(i2cFd, distance, num_anchors);
+  ret = getDists(i2cFd, distance);
   if (ret < 0) {
     return ret;
   }
@@ -116,7 +123,20 @@ int readMeasurement(int i2cFd, uint16_t* anchorId, float* distance, int num_anch
   return 0;
 }
 
+void getValidMeasurement(uint16_t* anchorId, float* distance,
+                        vector<uint16_t>& validAnchors, vector<float>& validDistance) {
+  validAnchors.clear();
+  validDistance.clear();
+  for (int i = 0; i < NUM_ANCHORS; i++) {
+    if (anchorId[i] && distance[i]) {
+      validAnchors.push_back(anchorId[i]);
+      validDistance.push_back(distance[i]);
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
+  int ret = 0;
   if (argc < 2) {
      printUsage();
      return EINVAL;
@@ -138,29 +158,69 @@ int main(int argc, char* argv[]) {
       cout << "Somethings wrong" << endl;
     }
   }
-  if (!strcmp(argv[1], "read")) {
+
+  if (!strcmp(argv[1], "calc")) {
     cout << "Reading measurement..." << endl;
-    int ret = readMeasurement(i2cFd, anchorId, distance, NUM_ANCHORS);
+    ret = readMeasurement(i2cFd, anchorId, distance);
     if (ret == -EBUSY) {
       cout << "Resource busy. Maybe ranging is in progress. Try again later" << endl;
-      return ret;
+      goto out;
     }
     if (ret == -EINVAL) {
       cout << "Somethings wrong" << endl;
-      return ret;
+      goto out;
     }
     if (ret < 0) {
       cout << "Somethings wrong" << endl;
-      return ret;
+      goto out;
     }
 
+    ifstream anchorFile("anchors.csv");
+    vector<int> anchorIdsInFile;
+    vector<vector<float>> anchorPosition;
+    string value;
+    while (anchorFile.good()) {
+      getline(anchorFile, value, ',');
+      anchorIdsInFile.push_back(stoi(value));
+
+      vector<float> pos;
+      for (int i = 0; i < 3; i++) {
+        getline(anchorFile, value, ',');
+        pos.push_back(stof(value));
+      }
+      anchorPosition.push_back(pos);
+    }
     for (int i = 0; i < NUM_ANCHORS; i++) {
       cout << "Anchor ID: " << anchorId[i] << " (0b" << bitset<16>(anchorId[i]) << ")"
            << ", Distnace: " << distance[i] << " (0b" << bitset<32>(*(uint32_t*)&distance[i]) << ")" << endl;
     }
+    vector<uint16_t> validAnchors;
+    vector<float> validDistance;
+    getValidMeasurement(anchorId, distance, validAnchors, validDistance);
+    if (validAnchors.size() < 3) {
+      cout << "Ranging measurements with anchors fewer than 3. Exit" << endl;
+      goto out;
+    }
+    MatrixXd anchors(validAnchors.size(), 3);
+    VectorXd ranges(anchors.rows());
+    for (int i = 0; i < anchors.rows(); i++) {
+      int idx;
+      for (int j = 0; j < anchorIdsInFile.size(); j++) {
+        if (validAnchors[i] == anchorIdsInFile[j]) {
+          idx = j;
+          break;
+        }
+      }
+      anchors(i, 0) = anchorPosition[idx][0];
+      anchors(i, 1) = anchorPosition[idx][1];
+      anchors(i, 2) = anchorPosition[idx][2];
+      ranges(i) = validDistance[i];
+    }
   }
+
+out:
   close(i2cFd);
 
-  return 0;
+  return ret;
 }
 
