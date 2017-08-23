668e5cadca6c8f1aebdca1feede3eb0ba60dd411
Seokseong Jeon <songsong@monet.postech.ac.kr>
2017-08-23 23:16:07 +0900

    Edit i2cController

--- tagRPi/i2cController.cpp
+++ tagRPi/i2cController.cpp
@@ -33,31 +33,34 @@ int triggerScan(int i2cFd) {
   cout << "Triggering scan..." << endl;
   if (write(i2cFd, &cmd_scan, 1) != 1) {
     cout << "Somethings wrong" << endl;
+    return -1;
   }
+  return 0;
 }
 
-int isReady(int i2cFd) {
+bool isReady(int i2cFd) {
   uint8_t data[32];
 
   cout << "Checking data is ready..." << endl;
   if (write(i2cFd, &cmd_data_ready, 1) != 1) {
     cout << "Write CMD_DATA_READY failed" << endl;
-    return -1;
+    return false;
   }
   if (read(i2cFd, data, 1) != 1) {
     cout << "Reading failed" << endl;
-    return -1;
+    return false;
   }
   if (data[0] == I2C_NODATA) {
     cout << "No data available" << endl;
-    return -EBUSY;
+    return false;
   }
   if (data[0] != I2C_DATARD) {
     cout << "Some other value than DATARD"
          << " (received: 0b" << bitset<8>(data[0])
          << ", expected: 0b" << bitset<8>(I2C_DATARD) << ")" << endl;
-    return -EINVAL;
+    return false;
   }
+  return true;
 }
 
 int getAnchorIds(int i2cFd, uint16_t* anchorId) {
@@ -65,16 +68,16 @@ int getAnchorIds(int i2cFd, uint16_t* anchorId) {
 
   cout << "Getting Anchors' IDs..." << endl;
   if (write(i2cFd, &cmd_type_id, 1) != 1) {
-    cout << "Writing CMD_TYPE_ID failed" << endl;
+    cout << "  Writing CMD_TYPE_ID failed" << endl;
     return -1;
   }
   if (read(i2cFd, data, 2 * NUM_ANCHORS) != 2 * NUM_ANCHORS) {
-    cout << "Reading Anchors' IDs failed" << endl;
+    cout << "  Reading Anchors' IDs failed" << endl;
     return -1;
   }
   for (int i = 0; i < NUM_ANCHORS; i++) {
     anchorId[i] = ID_NONE;
-    /*& Arduino uses little endian */
+    /* Arduino uses little endian */
     anchorId[i] = (data[2 * i + 1] << 8) | data[2 * i + 0];
   }
 }
@@ -92,10 +95,12 @@ int getDists(int i2cFd, float* distance) {
     return -1;
   }
   for (int i = 0; i < NUM_ANCHORS; i++) {
-    /* Arduino uses little endian */
+    /*
+     * Arduino uses little endian
+     *  Tricky part to assign float value via binary representation
+     */
     uint32_t float_binary = (data[4 * i + 3] << 24) | (data[4 * i + 2] << 16)
                           | (data[4 * i + 1] <<  8) | (data[4 * i + 0]      );
-
     distance[i] = *(float*)&float_binary;
   }
 }
@@ -104,9 +109,8 @@ int readMeasurement(int i2cFd, uint16_t* anchorId, float* distance) {
   int ret;
   uint8_t data[32];
 
-  ret = isReady(i2cFd);
-  if (ret < 0) {
-    return ret;
+  if(!isReady(i2cFd)) {
+    return -1;
   }
 
   ret = getAnchorIds(i2cFd, anchorId);
@@ -122,3 +126,14 @@ int readMeasurement(int i2cFd, uint16_t* anchorId, float* distance) {
   return 0;
 }
 
+void getValidMeasurement(uint16_t* anchorId, float* distance,
+                        vector<uint16_t>& validAnchors, vector<float>& validDistance) {
+  validAnchors.clear();
+  validDistance.clear();
+  for (int i = 0; i < NUM_ANCHORS; i++) {
+    if (anchorId[i] && distance[i]) {
+      validAnchors.push_back(anchorId[i]);
+      validDistance.push_back(distance[i]);
+    }
+  }
+}
--- tagRPi/i2cController.h
+++ tagRPi/i2cController.h
@@ -2,6 +2,7 @@
 #define I2C_CONTROLLER_H
 
 #include <cstdint>
+#include <vector>
 
 #define I2CDEV "/dev/i2c-1"
 #define I2CSLAVEADDR 0x04
@@ -12,10 +13,13 @@ using namespace std;
 
 int openI2C(char* i2cdev, int i2cslaveaddr);
 int triggerScan(int i2cFd);
-int isReady(int i2cFd);
+bool isReady(int i2cFd);
 int getAnchorIds(int i2cFd, uint16_t* anchorId);
 int getDists(int i2cFd, float* distance);
 int readMeasurement(int i2cFd, uint16_t* anchorId, float* distance);
+void getValidMeasurement(uint16_t* anchorId, float* distance,
+                         vector<uint16_t>& validAnchors,
+                         vector<float>& validDistance);
 
 #endif /* I2C_CONTROLLER_H */
 
