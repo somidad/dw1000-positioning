65da5238cd52a931a3961ca13b785f65b1dd788a
Seokseong Jeon <songsong@monet.postech.ac.kr>
2018-01-07 21:51:30 +0900

    Edit headers

--- headers/def.h
+++ headers/def.h
@@ -1,8 +1,11 @@
 #ifndef __DEF_H
 #define DEF_H
 
+// All DWM1000 anchors and tags have 2-byte long address
 #define ADDR_SIZE 2
+// No anchors nor tags have address 0. It is address of 'nothing'
 #define ID_NONE 0
+// 0xF000 is a flag bit indicating that an addres is tag's
 #define FLAG_TAG (1 << 15)
 
 #endif /* DEF_H */
